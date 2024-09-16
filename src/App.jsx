970fd0c412360f1592684a93ae95b7b30d3962bf
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2024-09-16 14:08:30 +0600

    feat: create structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-center text-primaryCol">
+        zynmart is loading...!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryCol: "#ee08e7",
+      },
+    },
+  },
+  plugins: [],
+};
